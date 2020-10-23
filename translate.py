32ca4f85a737e3c87b09cad465d488baf16af3ce
sagizaidor <71097154+sagizaidor@users.noreply.github.com>
2020-10-23 20:06:27 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -4,7 +4,6 @@ from google.cloud import translate_v2 as translate
 
 class Translate:
     def __init__(self, text, target):
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(os.getcwd(), 'googlekey.json')
         trns_client = translate.Client()
         output = trns_client.translate(text, target_language=target)
         self.output = output['translatedText']

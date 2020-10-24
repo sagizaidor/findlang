1d8f99f8723d9c7a133614ef9a412d00250eca87
sagizaidor <71097154+sagizaidor@users.noreply.github.com>
2020-10-24 15:41:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def get_contries(lang_or_name, by_name=False):
         except IndexError:
             return 'סליחה, אבל לא הצלחנו למצוא את המדינה. האם הקלדת נכון את שם המדינה?'
         lang = convert_lang_to_code(code, res='language')
+        print(lang)
         return translate(lang, 'he')
     contries = []
     for country in res['Response']:
@@ -65,6 +66,7 @@ def find():
     whereTheySpeak = request.form['whereTheySpeak']
     if whereTheySpeak:
         resWhere = get_contries(whereTheySpeak)
+        print(resWere)
     whichLangAt = request.form['whichLangAt']
     if whichLangAt:
         resWhich = get_contries(whichLangAt, by_name=True)

383d0854aa3aa78cb082e70e76d92a4fbcad88e8
sagizaidor <71097154+sagizaidor@users.noreply.github.com>
2020-10-24 15:59:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import os
 URL = 'http://countryapi.gear.host/v1/Country/getCountries?p'
 
 def convert_lang_to_code(lang, res='code'):
-    with open('lang&code.csv', 'r') as lang_code:
+    print(hi)
+    with open('langCode.csv', 'r') as lang_code:
+        print(ho)
         for _ in range(500):
             try:
                 code, language = lang_code.readline().split(',')
